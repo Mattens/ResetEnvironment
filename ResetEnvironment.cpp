7afdc018f573618c97ad1fc43a796de484093eb9
Mattens <Martin.Seck@Hotmail.de>
2020-02-14 18:54:33 +0100

    Projektdateien hinzufügen.

---
+++ ResetEnvironment.cpp
@@ -0,0 +1,46 @@
+/*|---------------------------------------------------------------------|
+  |                                                                     |
+  |  ResetEnvironment.cpp   © DVSeck http://emesco.de                   |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |                                                                     |
+  |---------------------------------------------------------------------|
+  |                                                                     |
+  |  created:           09.12.2015 se                                   |
+  |  letzte Aenderung:  13.07.2019 se                                   |
+  |                                                                     |
+  |---------------------------------------------------------------------|*/
+
+
+#include "StdAfx.h"
+
+
+int APIENTRY wWinMain ( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+{
+
+    DWORD   dwReturnValue;
+    BOOL    ok;
+
+    //SendMessageW ( HWND_BROADCAST, WM_SETTINGCHANGE, 0, ( LPARAM ) L"Environment" );
+
+    ok =
+        SendMessageTimeoutW
+        (
+            HWND_BROADCAST,
+            WM_SETTINGCHANGE,
+            0,
+            ( LPARAM ) L"Environment",
+            SMTO_ABORTIFHUNG,
+            5000,
+            ( PDWORD_PTR ) & dwReturnValue
+        );
+    
+    return 0;
+
+}
