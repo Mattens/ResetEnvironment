d69369f4c7dd38eb983177ca35ac633937f9be83
SECK Martin <martin.seck@hexagon.com>
2020-02-19 15:08:02 +0100

    Changes

--- ResetEnvironment.cpp
+++ ResetEnvironment.cpp
@@ -23,7 +23,7 @@
 int APIENTRY wWinMain ( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
 {
 
-    DWORD   dwReturnValue;
+    DWORD   dwReturnValue = 0;
     BOOL    ok;
 
     //SendMessageW ( HWND_BROADCAST, WM_SETTINGCHANGE, 0, ( LPARAM ) L"Environment" );
